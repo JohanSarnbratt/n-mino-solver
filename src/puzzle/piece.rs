d5098b569a3684a7c9dd65521127f71e98c443b6
Johan Särnbratt <johan@minna.tech>
2021-12-15 18:10:06 +0100

    Enable turn for piece

--- src/puzzle/piece.rs
+++ src/puzzle/piece.rs
@@ -3,7 +3,21 @@ use std::fmt::Formatter;
 
 pub struct Piece {
     pub coords: Vec<(i32, i32)>,
+    max_x: i32,
+    max_y: i32,
     pub name: char
+}
+
+impl Piece {
+    fn turn(&self) -> Piece {
+        let new_coords = self.coords.iter().map(|(x,y)| -> (i32,i32) { return (self.max_y - *y, *x); }).collect();
+        Piece {
+            coords: new_coords,
+            max_x: self.max_y,
+            max_y: self.max_x,
+            name: self.name
+        }
+    }
 }
 
 impl std::fmt::Display for Piece {
@@ -23,6 +37,12 @@ impl std::fmt::Display for Piece {
     }
 }
 
+pub fn construct_piece(coords: Vec<(i32, i32)>, name: char) -> Piece {
+    use std::cmp::max;
+    let maxs = coords.iter().fold((0i32,0i32), |m: (i32, i32), p: &(i32, i32)| { (max(m.0, p.0), max(m.1, p.1)) });
+    return Piece { coords, name, max_x: maxs.0, max_y: maxs.1};
+}
+
 pub fn pieces() -> Vec<Piece> {
     let p1 = piece_t();
     let p2 = piece_z();
@@ -37,9 +57,9 @@ pub fn test_pieces() -> Vec<Piece> {
 }
 
 fn piece_t() -> Piece {
-    return Piece { coords: vec![(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)], name: 'T' };
+    return construct_piece(vec![(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)],  'T');
 }
 
 fn piece_z() -> Piece {
-    return Piece { coords: vec![(0, 0), (0, 1), (1, 1), (1, 2)], name: 'Z' };
+    return construct_piece(vec![(0, 0), (0, 1), (1, 1), (1, 2)], 'Z');
 }
